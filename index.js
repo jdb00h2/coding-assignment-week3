18856af1193f411465492e63541c6ca72dfa181c
Jeffery Bush <jefferybush17@gmail.com>
2022-02-09 23:26:15 -0600

    compete?

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ console.log(sum);
 
 // // 7.	Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
 
+function concat(word, n){
+    let output = '';
+    for (let i = 0; i < n; i++){
+        output += word;
+    }
+    return output;
+}
+
 
 // // 8.	Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
 
@@ -83,37 +91,79 @@ function fullName(firstName, lastName){
 // // 9.	Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
 
 function greaterThan(inputArray){
-    for(let i = 0; i < inputArray.length; i++){
-    sum += inputArray[i]; 
-    } 
+    let sum = inputArray.reduce(function (previousValue, currentValue){
+        return previousValue + currentValue
+    })
     if (sum > 100){
-    return true
-    } 
+        return true;
+    }
 }
 
 // // 10.	Write a function that takes an array of numbers and returns the average of all the elements in the array.
 
-function average(x){
-    for(let i = 0; i < x.length; i++){
-        total += x[i]; 
-        mean = total/x.length
-    return mean
-    }
+function average(inputArray){
+    let sum = inputArray.reduce(function (previousValue, currentValue){
+        return previousValue + currentValue;
+    })
+    return sum/inputArray.length;
 }
-console.log(nameLengths);
-let test = average(nameLengths);
-console.log(test)
-
 
 // // 11.	Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
+function largerAverage(inputArray1, inputArray2){
+    let sum1 = inputArray1.reduce(function (previousValue, currentValue){
+        return previousValue + currentValue;
+    })
+    let mean1 = sum1/inputArray1.length;
+    let sum2 = inputArray2.reduce(function(previousValue, currentValue){
+        return previousValue + currentValue;
+    })
+    let mean2 = sum2/inputArray2.length;
+    if(mean1 > mean2){
+        return true
+    }
+}
 
 // // 12.	Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 
 function willBuyDrink(isHotOutside, moneyInPocket){
     if (isHotOutside && moneyInPocket > 10.50){
-        return('true')
+        return true
     }
 }
 
+
 // // 13.	Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+
+// This function compares the scorecards from two players after a round of mini golf. It accepts the input as arrays with 18 values
+// to represent the eighteen holes played. I created this function because I have fun memories of playing mini golf and it seemed 
+// like a reasonable way to test what I've learned this week. 
+
+playerOneScores = [3, 4, 3, 5, 8, 2, 4, 4, 5, 3, 6, 4, 5, 5, 3, 4, 2, 5]
+playerTwoScores = [2, 3, 4, 3, 5, 6, 4, 6, 5, 2, 6, 6, 4, 6, 1, 5, 5, 4]
+
+playerOne = 'Jeffery'
+playerTwo = 'Marina'
+function whoWins(player1, player2){
+    if(player1.length !== 18 || player2.length !== 18){
+        return 'Invalid scorecard.'
+    }
+    let sum1 = player1.reduce(function(previousValue, currentValue){
+        return previousValue + currentValue;
+    })
+    let sum2 = player2.reduce(function(previousValue, currentValue){
+        return previousValue + currentValue;
+    })
+    if(sum1 == sum2){
+        return 'Its all knotted up, into the playoff you go!'
+    } else if(sum1 < sum2){
+        let win = sum2 - sum1
+        return `${playerOne} wins by ${win}!`
+    } else {
+        let win = sum1 - sum2
+        return `Player Two wins by ${win}!`
+    }
+}
+
+let finalTest = whoWins(playerOneScores, playerTwoScores);
+console.log(finalTest);
